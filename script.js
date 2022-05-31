5bc2cb4353853400a6183ca93dca63d8d2590956
grilledcheeseplease <lacynunnally402@gmail.com>
2022-05-31 18:10:53 -0400

    making sure all css classes are properly assigned

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 // variables 
 var currentDate = moment().format("dddd, MMMM Do");
 // console.log(currentDate);
+var timeBlock = $('.time-block')
+
+// date at top of calendar 
+$('#currentDay').append(currentDate);
+
+// color code blocks for past(gray), present(red), and future(green)
+function blockColors () {
+    var currentTime = moment().hours();
+    $(".time-block").each(function() {
+        var timeLine = parseInt($(this).attr("id"));
+        if (timeLine > currentTime) {
+            $(this).addClass('past');
+        } else if (timeLine === currentTime) {
+            $(this).removeClass('past');
+            $(this).addClass('present');
+            $(this).removeClass('future');
+        } else {
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+            $(this).addClass('future');
+        }
+    })  
+};
+
+blockColors();
+
