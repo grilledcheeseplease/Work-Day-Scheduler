1aa2d59a6ac1ddd9ee0fc1f4b6fb04fd02409994
grilledcheeseplease <lacynunnally402@gmail.com>
2022-05-31 19:38:47 -0400

    trying to fix col and rows

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 // variables 
-var currentDate = moment().format("dddd, MMMM Do");
+var currentDate = moment().format('dddd, MMMM Do');
 // console.log(currentDate);
-var timeBlock = $('.time-block')
+
 
 // date at top of calendar 
 $('#currentDay').append(currentDate);
 
 // color code blocks for past(gray), present(red), and future(green)
 function blockColors () {
+    
     var currentTime = moment().hours();
-    $(".time-block").each(function() {
-        var timeLine = parseInt($(this).attr("id"));
+    var timeBlock = $('.time-block');
+    
+    $('.time-block').each(function() {
+
+        var timeLine = parseInt($(this).attr('id'));
+
         if (timeLine > currentTime) {
             $(this).addClass('past');
+
         } else if (timeLine === currentTime) {
             $(this).removeClass('past');
             $(this).addClass('present');
             $(this).removeClass('future');
+
         } else {
             $(this).removeClass('past');
             $(this).removeClass('present');
@@ -27,3 +34,15 @@ function blockColors () {
 
 blockColors();
 
+
+
+// save btn event listener for localStorage
+$('.saveBtn').on('click', function() {
+    console.log(this);
+    var text = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
+    localStorage.setItem(time, text);
+},(getItemsFromStorage, function() {
+    $(this).siblings('.description').val(text);
+    $(this).parent().attr('id', time)
+}));
